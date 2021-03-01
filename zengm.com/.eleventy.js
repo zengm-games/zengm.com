e473b1da76539ce8732e135f47c2781335ec2747
Jeremy Scheff <jdscheff@gmail.com>
2021-03-01 15:00:02 -0500

    Minify HTML output

--- zengm.com/.eleventy.js
+++ zengm.com/.eleventy.js
@@ -1,4 +1,5 @@
 const CleanCSS = require("clean-css");
+const htmlmin = require("html-minifier");
 const { minify } = require("terser");
 
 module.exports = function(eleventyConfig) {
@@ -20,6 +21,21 @@ module.exports = function(eleventyConfig) {
             // Fail gracefully.
             callback(null, code);
         }
+    });
+
+
+    eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+        // Eleventy 1.0+: use this.inputPath and this.outputPath instead
+        if(outputPath.endsWith(".html")) {
+            let minified = htmlmin.minify(content, {
+                useShortDoctype: true,
+                removeComments: true,
+                collapseWhitespace: true
+            });
+            return minified;
+        }
+
+        return content;
     });
 
     return {
