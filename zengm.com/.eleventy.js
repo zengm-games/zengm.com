fe6aa115fbd950eee7a62bf118568975cfddc928
Jeremy Scheff <jdscheff@gmail.com>
2021-03-01 14:45:58 -0500

    Inline minified CSS

--- zengm.com/.eleventy.js
+++ zengm.com/.eleventy.js
@@ -1,5 +1,11 @@
+const CleanCSS = require("clean-css");
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/files");
+
+    eleventyConfig.addFilter("cssmin", function(code) {
+        return new CleanCSS({}).minify(code).styles;
+    });
 
     return {
         dir: {
