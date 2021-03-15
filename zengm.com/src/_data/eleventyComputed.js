dfc640858ea0f50e65cbff8b7fa9e9434d8bec0f
Jeremy Scheff <jdscheff@gmail.com>
2021-03-14 23:09:29 -0400

    Infer sport from URL

--- zengm.com/src/_data/eleventyComputed.js
+++ zengm.com/src/_data/eleventyComputed.js
@@ -1,6 +1,19 @@
 const bySport = require("../util/bySport");
 
 module.exports = {
+    sport: data => {
+        if (data.page.filePathStem.startsWith("/basketball/")) {
+            return "basketball";
+        }
+        if (data.page.filePathStem.startsWith("/football/")) {
+            return "football";
+        }
+        if (data.page.filePathStem.startsWith("/hockey/")) {
+            return "hockey";
+        }
+        return undefined;
+    },
+    
     gameName: data => data.sport ? bySport({
         basketball: "Basketball GM",
         football: "Football GM",
