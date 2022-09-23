db81d5853676ae0cd3ce1f85925f3bd4eefc7bcc
Jeremy Scheff <jdscheff@gmail.com>
2022-09-23 12:43:57 -0400

    Remove extra header

--- src/sport-specific/manual/worker-console.11ty.js
+++ src/sport-specific/manual/worker-console.11ty.js
@@ -34,8 +34,6 @@ You can easily access a simple version of the worker console at <span class="tex
 3. Scroll down (maybe way down) until you see the "Shared Workers" section. There should be an entry there similar to /gen/worker-2020.08.02.0001.js.
 4. Click "Inspect" next to it. That will open a new tab where you can run the code snippets below.
 
-## Browser compatibility
-
 **Only needed for Safari 14 or later, and very old versions of other browsers!** If your code uses the keyword \`await\` (which is almost always true), you need to wrap it in an async function. That means you need to put \`(async () => {\` at the beginning of the code and \`})();\` at the end.
 
 ## Code snippets
